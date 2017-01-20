b14066aa45524439ea982c469fd4f90aa4706e5a
redperov <redperov@gmail>
2017-01-20 22:10:31 +0200

    Working version without logs, but commented.

--- Server.cpp
+++ Server.cpp
@@ -4,12 +4,18 @@
 #include "sockets/Tcp.h"
 #include "src/StandardVehicle.h"
 #include "src/Menu.h"
+//#include <boost/log/core.hpp>
+//#include <boost/log/trivial.hpp>
+//#include <boost/log/expressions.hpp>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
+    //Print only logging warning or higher level
+    //boost::log::core::get()->set_filter(
+    //boost::log::trivial::severity >= boost::log::trivial::warning);
     //Create the socket.
     Socket *socket = new Tcp(true, atoi(argv[1]));
-    Menu menu(socket);
+    Menu   menu(socket);
 
     //Initializes and runs the menu.
     menu.initializeGame();
